f4a331ed65528fd6281a168037734d9e6e26ccfb
Guilherme <guiilherme.ambrosio@gmail.com>
2025-09-24 14:58:28 -0300

    versão um

---
+++ prog7.js
@@ -0,0 +1,24 @@
+let resposta = document.getElementById('resposta')
+
+function principal(){
+    let nota1 = Number(document.getElementById('nota1').value)
+    let nota2 = Number(document.getElementById('nota2').value)
+    let nota3 = Number(document.getElementById('nota3').value)
+
+    let media = (nota1 + nota2 + nota3) / 3.0
+
+    console.log(`As notas do aluno são: ${nota1} ${nota2} ${nota3}`)
+    console.log(`A média do aluno foi: ${media.toFixed(2)}`)
+
+    if (media < 5.0){
+        resposta.innerHTML = `Você não alcançou a média: REPROVADO`
+    }else if(media >= 5.0 && media < 7.0){
+        resposta.innerHTML = `Precisa fazer a RECUPERAÇÃO`
+    }else if(media >= 7.0){
+        resposta.innerHTML = `Você está APROVADO, Parabéns!`
+    }
+
+    // resposta.innerHTML = ``
+    // resposta.innerHTML += `As notas do aluno são: ${nota1} ${nota2} ${nota3} <br>`
+    // resposta.innerHTML += `A média do aluno foi: ${media.toFixed(2)}`
+}
